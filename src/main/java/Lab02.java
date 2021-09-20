d3011d5cc45131d8f3abbadd627c07e3b65cc8aa
Kxssoon <90279486+Kxssoon@users.noreply.github.com>
2021-09-20 11:18:23 -0400

    Update Lab02.java

--- src/main/java/Lab02.java
+++ src/main/java/Lab02.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Lab02 {
 
 	// YOUR CODE GOES HERE
-
+    public static double[] map(UnaryFunction unaryFunction, double[] numArray){
+        double[] resultArr = new double[numArray.length];
+        int count = 0;
+        for (double num : numArray){
+            resultArr[count] = unaryFunction.exec(num);
+            count++;
+        }
+        System.out.println(Arrays.toString(resultArr));
+        return resultArr;
+    }
 }
